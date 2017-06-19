3080004e1799cd290921f5c877c3bc526af2bc74
NightcoreSergeant <tomazic.tadej@gmail.com>
2017-06-19 19:29:27 +0200

    update

--- Python/all ex/ex25.py
+++ Python/all ex/ex25.py
@@ -15,14 +15,14 @@ def print_first_word(words):
 def print_last_word(words):
 	"""Prints last word after popping it off"""
 	word = words.pop(-1)
-	print word
+	print(word)
 
 def sort_sentence(sentence):
 	"""Takes in a full sentence and returns the sorted words."""
 	words = break_words(sentence)
 	return sort_words(words)
 
-def print_first_and_last(sentence)
+def print_first_and_last(sentence):
 	"""Prints the first and last words of the sentence."""
 	words = break_words(sentence)
 	print_first_word(words)
